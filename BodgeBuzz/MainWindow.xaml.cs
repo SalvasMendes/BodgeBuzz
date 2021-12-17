ea532280cfad6d6add9b7772646adb7308885c6b
SrMendes <sr.mendes@campus.fct.unl.pt>
2021-12-17 13:03:05 +0000

    1st commit

---
+++ BodgeBuzz/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace BodgeBuzz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Test shit to see if git works
+        }
+    }
+}
